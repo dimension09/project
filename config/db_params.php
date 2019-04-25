04010a954a191ec2b40fe35500c2d0b31478a1b8
dimension09 <diamondswif@gmail.com>
2019-04-25 14:53:43 +0300

    1.0

---
+++ config/db_params.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'host' => 'localhost',
+    'dbname' => 'project',
+    'user' => 'root',
+    'password' => '111',            
+);
---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    
+    'product/([0-9]+)' => 'product/view/$1', // actionView в ProductController
+
+    
+    '' => 'site/index', // actionIndex в SiteController
+    
+);
